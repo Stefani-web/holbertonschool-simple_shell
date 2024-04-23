f7585a42e5b946db561041fa05f569271d9b5960
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-04-23 22:19:38 +0200

    add doc for tokenization
    Co-authored-by: Stefani-web <Stefani-web@users.noreply.github.com>

--- split_string.c
+++ split_string.c
@@ -8,10 +8,11 @@
  */
 char **split_string(char *line)
 {
-	int index = 0;
-	char *tmp_line = malloc(strlen(line) + 1);
-	char *token;
-	char **args_array = malloc(sizeof(char *) * MAX_ARGS);
+	int index = 0; /* Index for the array of arguments */
+	char *tmp_line = malloc(strlen(line) + 1); /* Allocate memory for the temporary line */
+	char *token; /* Pointer to store the token */
+
+	char **args_array = malloc(sizeof(char *) * MAX_ARGS); /* Allocate memory for the array of arguments */
 
 	if (tmp_line == NULL || args_array == NULL)
 	{
@@ -22,15 +23,17 @@ char **split_string(char *line)
 	token = strtok(line, " \t\n");
 	while (token)
 	{
-		args_array[index] = malloc(strlen(token) + 1);
+		args_array[index] = malloc(strlen(token) + 1); /* Allocate memory for the token */
 		if (args_array[index] == NULL)
-			perror("Error on malloc"), exit(EXIT_FAILURE);
-
-		strcpy(args_array[index], token);
-		token = strtok(NULL, " \t\n");
-		index++;
+		{
+			perror("Error on malloc");
+			exit(EXIT_FAILURE);
+		}
+		strcpy(args_array[index], token); /* Copy the token to the array */
+		token = strtok(NULL, " \t\n"); /* Move to the next token */
+		index++; /* Increment the index */
 	}
-	args_array[index] = NULL;
-	free(tmp_line);
-	return (args_array);
+	args_array[index] = NULL; /* Set the last element of the array to NULL */
+	free(tmp_line);	/* Free the temporary line */
+	return (args_array); /* Return the array of arguments */
 }
