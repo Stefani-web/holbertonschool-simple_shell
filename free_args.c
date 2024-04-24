1ce5c1ce0b4f59622338f71bb0b226fb0d669e1a
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-04-24 22:24:50 +0200

    add feof function in simple_shell.c and update free_args file for more lisibility
    Co-authored-by: Stefani-web <Stefani-web@users.noreply.github.com>

--- free_args.c
+++ free_args.c
@@ -11,9 +11,11 @@ void free_args(char **strings_array)
 
 	if (strings_array == NULL)
 		return;
+
 	/* Iterate through the array of strings */
 	for (index = 0; strings_array[index] != NULL; index++)
 		free(strings_array[index]); /* Free the memory allocated for each string */
+
 	/* Free the memory allocated for the array of strings itself */
 	free(strings_array);
 }
--- simple_shell.c
+++ simple_shell.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	int command_count = 0; /* Counter for number of commands entered by user */
 	char *input_line = NULL; /* Pointer to store the input command line */
-	char **command_args = NULL; /* Pointer to store command and its arguments */
-	size_t length = 0; /* Length of the input line */
+	char **command_args; /* Pointer to store command and its arguments */
+	size_t length_input = 0; /* Length of the input line */
 	ssize_t bytes_read; /* Number of bytes read by getline() */
 	int should_continue = 1; /* Flag to continue the loop */
 
@@ -18,25 +18,35 @@ int main(void)
 	{
 		if (isatty(STDIN_FILENO)) /* Check if the shell is in interactive mode */
 			printf("HolbitesHell$ "); /* Print the prompt */
-
-		bytes_read = getline(&input_line, &length, stdin); /* Read the input line */
+		/* Read the input line */
+		bytes_read = getline(&input_line, &length_input, stdin);
 
 		if (bytes_read == -1) /* Check if getline() failed */
 		{
-			should_continue = 0;
-			break;
-		}
-		input_line[bytes_read - 1] = '\0'; /* Remove \n from end of input line */
-
-		if (strcmp(input_line, "exit") == 0) /* Check if the user entered "exit" */
-			break;
-		/* Split the input line into command and arguments */
-		command_args = split_string(input_line);
-		if (command_args[0] == NULL) /* Check if the command is empty */
-		{
-			free_args(command_args); /* Free the command arguments */
+			if (feof(stdin))
+			{
+				if (isatty(STDIN_FILENO))
+					printf("\n");
+				fflush(stdout);
+				break;
+			}
+			free(input_line);
 			continue;
 		}
+		else
+		{
+			/* Split the input line into command and arguments */
+			command_args = split_string(input_line);
+			if (command_args[0] == NULL) /* Check if the command is empty */
+			{
+				free_args(command_args); /* Free the command arguments */
+				continue;
+			}
+		}
+		/* Check if the user entered "exit" */
+		if (strcmp(input_line, "exit") == 0)
+			break;
+
 		execute_command(command_args, ++command_count); /* Execute the command */
 		free_args(command_args); /* Free the command arguments */
 	}
