3c737e6473653d6c910aa044a6fc7afad8fdba49
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-04-24 22:55:45 +0200

    fix error on entire path command
    Co-authored-by: Stefani-web <Stefani-web@users.noreply.github.com>

--- path.c
+++ path.c
@@ -12,25 +12,26 @@ char *get_path(char *input_line)
 	char *path = getenv("PATH"); /* Get the PATH environment variable */
 	char *token, *complete_path;
 	char *tmp_path = strdup(path); /* Duplicate the PATH string to tokenize */
-	struct stat st;
 
 	if (input_line == NULL)
 		exit(EXIT_FAILURE);
+	/* Check if the file exists and is executable */
+	if (access(input_line, F_OK | X_OK) == 0)
+		return (input_line); /* Return the input_line if it is found */
 
 	token = strtok(tmp_path, ":"); /* Tokenize the PATH string */
 	while (token)
 	{
 		complete_path = malloc(strlen(token) + strlen(input_line) + 2);
-		if (complete_path == NULL) /* Check if memory allocation for file_path failed */
+		if (complete_path == NULL) /* Check if malloc failed */
 		{
-			perror("Malloc failed");
 			free(tmp_path); /* Free the memory allocated for tmp_path */
 			return (NULL); /* Return NULL if memory allocation failed */
 		}
 		/* Concatenate the token and input_line to get the complete path */
 		sprintf(complete_path, "%s/%s", token, input_line);
-
-		if (stat(complete_path, &st) == 0)
+		/* Check if the file exists and is executable */
+		if (access(complete_path, F_OK | X_OK) == 0)
 		{
 			free(tmp_path);
 			return (complete_path);
